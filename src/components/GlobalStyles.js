5522daa46655ea8651e5e83d080da18f7e612897
gautiermarechal <gaut.marechal@gmail.com>
2021-01-10 20:49:21 +0100

    Added categorized tweets but still need to figure out how to identify which user categiorized the tweet

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -3,6 +3,9 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyles = createGlobalStyle`
 a:link    { color: white; }
 a:visited { color: white; }
+a:hover{
+    text-decoration: none;
+}
 `;
 
 export default GlobalStyles;
--- src/components/MyCategorizedTweets.js
+++ src/components/MyCategorizedTweets.js
@@ -1,63 +1,43 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import api from "../api/index";
-import Tweet from "./Tweet";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import SearchBar from "./SearchBar";
+import TweetsGrid from "./TweetsGrid";
+import { useSelector } from "react-redux";
+import apis from "../api";
 
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const MyCategorizedTweets = ({ currentUser }) => {
+const MyCategorizedTweets = () => {
   const [tweets, setTweets] = useState([]);
-  const [tweetsToDisplay, setTweetsToDisplay] = useState([]);
+  const currentUser = useSelector((state) => state.currentUser);
 
   useEffect(() => {
-    console.log(currentUser);
-    let tweets = [];
-    let tweetsToDisplay = [];
-    const get = async () => {
-      tweets = await api.getAllTweets();
-      setTweets(tweets.data);
-    };
-    get();
-  }, []);
+    if (!currentUser) {
+      return;
+    }
+
+    apis.getAllTweetsFromUser(currentUser.currentUser.id).then((res) => {
+      console.log(res);
+      setTweets(res.data.data);
+    });
+  }, [currentUser]);
 
   return (
     <>
-      <Wrapper>
-        <Container>
-          <Row>
-            {tweetsToDisplay &&
-              tweetsToDisplay.map((tweet) => {
-                return (
-                  <Col lg={true} lg={4} style={{ marginTop: "20px" }}>
-                    <Tweet
-                      key={tweet.id}
-                      userName={tweet.user_name}
-                      userUserName={tweet.user_screen_name}
-                      userImageUrl={tweet.user_image_url}
-                      tweetCategory={tweet.tweet_organized_category}
-                      tweetDate={tweet.tweet_organized_date}
-                      tweetContent={
-                        tweet.tweet_organized_content
-                          ? tweet.tweet_organized_content[0]
-                          : tweet.tweet_organized_content
-                      }
-                      tweetContentExtended={tweet.tweet_organized_content}
-                    />
-                  </Col>
-                );
-              })}
-          </Row>
-        </Container>
-      </Wrapper>
+      {tweets ? (
+        tweets.length !== 0 ? (
+          <TweetsGrid tweets={tweets} />
+        ) : (
+          <Notweets>No tweets categorized yet</Notweets>
+        )
+      ) : (
+        <p>loading</p>
+      )}
     </>
   );
 };
+
+const Notweets = styled.div`
+  color: white;
+  width: 100%;
+  text-align: center;
+`;
 
 export default MyCategorizedTweets;
