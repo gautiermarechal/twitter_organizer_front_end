58b286d66366e8f2068f0157c892728ff47902c3
gautiermarechal <gaut.marechal@gmail.com>
2021-05-26 09:22:42 -0400

    (Add bookmarkedby attribute to tweets

--- src/components/TweetsGrid.js
+++ src/components/TweetsGrid.js
@@ -15,38 +15,42 @@ const TweetsGrid = ({ tweets }) => {
       <MainGridContainer>
         {tweets &&
           tweets.map((element) => {
-            const tweetIDFormatted =
-              `${element.tweet_organized_category}_` +
-              element.tweet_organized_id;
+            if (element) {
+              const tweetIDFormatted =
+                `${element.tweet_organized_category}_` +
+                element.tweet_organized_id;
 
-            return (
-              <Tweet
-                key={tweetIDFormatted}
-                id={element.tweet_organized_id}
-                userName={element.user_name}
-                userUserName={element.user_screen_name}
-                userImageUrl={element.user_image_url}
-                tweetCategory={element.tweet_organized_category}
-                tweetDate={element.tweet_organized_date}
-                tweetContent={
-                  element.tweet_organized_content
-                    ? element.tweet_organized_content[0]
-                    : element.tweet_organized_content
-                }
-                tweetContentExtended={element.tweet_organized_content}
-                isBookmarked={
-                  currentUser.status === "loggedIn"
-                    ? currentUser.currentUser.tweetsBookmarked === undefined
-                      ? false
-                      : currentUser.currentUser.tweetsBookmarked.includes(
-                          element.tweet_organized_id
-                        )
-                      ? true
+              return (
+                <Tweet
+                  key={tweetIDFormatted}
+                  id={element.tweet_organized_id}
+                  userName={element.user_name}
+                  userUserName={element.user_screen_name}
+                  userImageUrl={element.user_image_url}
+                  tweetCategory={element.tweet_organized_category}
+                  tweetDate={element.tweet_organized_date}
+                  tweetContent={
+                    element.tweet_organized_content
+                      ? element.tweet_organized_content[0]
+                      : element.tweet_organized_content
+                  }
+                  tweetContentExtended={element.tweet_organized_content}
+                  isBookmarked={
+                    currentUser.status === "loggedIn"
+                      ? currentUser.currentUser.tweetsBookmarked === undefined
+                        ? false
+                        : currentUser.currentUser.tweetsBookmarked.includes(
+                            element.tweet_organized_id
+                          )
+                        ? true
+                        : false
                       : false
-                    : false
-                }
-              />
-            );
+                  }
+                />
+              );
+            } else {
+              return;
+            }
           })}
       </MainGridContainer>
     </>
